d63fc9a4d261881a07cb1613ee7eed64894b6bdb
Marelle Palm <marelle.palm@gmail.com>
2026-02-16 09:28:51 +0200

    rimenu function

--- resources/js/pages/Plants/Show.vue
+++ resources/js/pages/Plants/Show.vue
@@ -16,6 +16,15 @@ type Plant = {
   next_fertilizing_label?: string | null
 }
 
+const showMenu =ref(false)
+function toggleMenu () {
+  showMenu.value = !showMenu.value
+}
+
+function closeMenu () {
+  showMenu.value = false
+}
+
 const { plant }= defineProps<{ plant: Plant }>()
 
 const markingWatered = ref(false)
@@ -37,10 +46,42 @@ function markWatered() {
 <template>
   <PlantDetail
     :plant="plant"
+    @menu="toggleMenu"
     :marking-watered="markingWatered"
     :just-watered="justWatered"
     @mark-watered="markWatered"
   />
+
+  
+<!-- MENU -->
+<div v-if="showMenu" class="fixed inset-0 z-50">
+  
+  <!-- overlay -->
+  <div
+    class="absolute inset-0"
+    @click="closeMenu"
+  ></div>
+
+  <!-- dropdown -->
+  <div
+    class="absolute right-6 top-20 w-56 rounded-2xl bg-white dark:bg-surface-dark shadow-xl border border-black/5 dark:border-white/10 overflow-hidden"
+    @click.stop
+  >
+    <button class="w-full text-left px-4 py-3 hover:bg-black/5 dark:hover:bg-white/5">
+      Muuda taim
+    </button>
+
+    <button class="w-full text-left px-4 py-3 hover:bg-black/5 dark:hover:bg-white/5">
+      Dubleeri
+    </button>
+
+    <button class="w-full text-left px-4 py-3 hover:bg-black/5 dark:hover:bg-white/5 text-red-600">
+      Kustuta
+    </button>
+  </div>
+
+</div>
+
 </template>
 
 
