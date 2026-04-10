b4490e25c4b9a5c958d10c1c3b1be7994008d6de
Marelle Palm <marelle.palm@gmail.com>
2026-04-10 09:38:17 +0300

    live fix

--- database/migrations/2026_04_10_054755_add_is_favorite_to_plants_table.php
+++ database/migrations/2026_04_10_054755_add_is_favorite_to_plants_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('plants', function (Blueprint $table) {
-            $table->boolean('is_favorite')->default(false)->after('image');
+           $table->boolean('is_favorite')->default(false);
         });
     }
 
